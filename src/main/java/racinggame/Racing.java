e3e9687669dc9efb1bc4cd2d052e6a77f1ab7a23
OHHYUN <omplcie@gmail.com>
2021-10-09 16:14:37 +0900

    feat(racing): 여러개의 자동차 전진

--- src/main/java/racinggame/Racing.java
+++ src/main/java/racinggame/Racing.java
@@ -1,7 +1,11 @@
 package racinggame;
 
+import static nextstep.utils.Randoms.*;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import nextstep.utils.Randoms;
 
 public class Racing {
 
@@ -16,5 +20,23 @@ public class Racing {
 
 	public List<Car> getCars() {
 		return this.cars;
+	}
+
+	public void moveAll() {
+		for(Car car : this.cars){
+			move(car);
+		}
+		printAll();
+	}
+
+	public void move(Car car){
+		int ranNo = Randoms.pickNumberInRange(0,9);
+		car.move(ranNo);
+	}
+
+	public void printAll(){
+		for(Car car : this.cars){
+			car.printDistance();
+		}
 	}
 }
