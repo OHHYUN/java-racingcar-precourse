2ed780a9c99ac259c5f552d2adf133aa73ff477c
OHHYUN <omplcie@gmail.com>
2021-10-09 15:30:20 +0900

    test(car): 자동차의 거리를 출력하는 함수 테스트

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -2,9 +2,23 @@ package racinggame;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import nextstep.test.NSTest;
+
 public class CarTest {
+
+	private OutputStream outputStream = new ByteArrayOutputStream();
+
+	@BeforeEach
+	void beforeEach(){
+		System.setOut(new PrintStream(outputStream));
+	}
 
 	@Test
 	void 자동차_전진() {
@@ -21,4 +35,13 @@ public class CarTest {
 		car.move(0);
 		assertThat(car.getDistance()).isEqualTo(0);
 	}
+
+	@Test
+	void 자동차_현재_거리_출력(){
+		Car car = new Car("Tony");
+		car.move(4);
+		car.printDistance();
+		assertThat(outputStream.toString().trim()).isEqualTo("Tony : -");
+	}
+
 }
