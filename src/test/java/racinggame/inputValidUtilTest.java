5080d65dec71367439452cfeb57f3b4217eb72cd
OHHYUN <omplcie@gmail.com>
2021-10-13 16:21:31 +0900

    refactor: 안쓰는 메소드 삭제

--- src/test/java/racinggame/inputValidUtilTest.java
+++ src/test/java/racinggame/inputValidUtilTest.java
@@ -103,8 +103,4 @@ public class inputValidUtilTest {
         outputStandard();
     }
 
-    private void command(final String... args) {
-        final byte[] buf = Strings.join(args).with("\n").getBytes();
-        System.setIn(new ByteArrayInputStream(buf));
-    }
 }
