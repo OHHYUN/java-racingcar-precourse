8d0f6eab355ce01a920a2588ea352f2fc9819901
OHHYUN <omplcie@gmail.com>
2021-10-13 16:08:04 +0900

    feat: 메인로직에서 메시지 출력하는 기능

---
+++ src/main/java/racinggame/model/MessageType.java
@@ -0,0 +1,16 @@
+package racinggame.model;
+
+public enum MessageType {
+    INPUT_CAR_NAME("자동차 이름을 입력해주세요."),
+    INPUT_RUNNING_TIME("시도할 횟수는 몇회입니까?");
+
+    private String msg;
+
+    MessageType(String msg){
+        this.msg = msg;
+    }
+
+    public void printMsg(){
+        System.out.println(this.msg);
+    }
+}
