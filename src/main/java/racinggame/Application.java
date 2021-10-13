262accc80c34a2cc77eee958c930ee6ae69b5d3e
OHHYUN <omplcie@gmail.com>
2021-10-13 16:06:20 +0900

    feat: 메인 로직추가

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,11 @@
 package racinggame;
 
+import racinggame.core.Race;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
+        Race race = new Race();
+        race.play();
     }
 }
